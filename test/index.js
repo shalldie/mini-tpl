9c66c5ec91fafd84418cf87de59ea726c7b15748
shalldie <394229785@qq.com>
2017-11-13 18:08:53 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -3,10 +3,11 @@ const tpl = require('../build/mini-tpl.min');
 const content = `
 <ul id="idid" name='namename'>
 <% for(var i=0; i<data.length; i++){
-    var word = "fdaf";
+    var word = "hello";
+    word += ' world';
     var item = data[i];
     if(item.age < 30){%>
-        <li data-age="<%=item.age%>">我的名字是<%=item.name%>，我的年龄是<%=item.age%></li>
+        <li data-world='<%=word%>' data-age="<%=item.age%>">我的名字是<%=item.name%>，我的年龄是<%=item.age%></li>
     <%}else{%>
         <li data-name="<%=item.name%>">my name is <%=item.name%>,my age is a sercet.</li>
     <%}%>
